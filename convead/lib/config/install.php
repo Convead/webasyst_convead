a4b7ddb7fc3c46cb18006c78c1b1ce94f9a95885
Pavel Bozhenik <bozhenik@gmail.com>
2025-07-14 14:32:27 +0300

    update files

--- convead/lib/config/install.php
+++ convead/lib/config/install.php
@@ -8,7 +8,7 @@
  * @author Vladimir Savelyev
  * @link http://convead.ru/
  * @copyright Copyright (c) 2025, Convead
- * @version    2.6, 2025-08-09
+ * @version    2.6.0, 2025-08-09
  *
  */
 
--- convead/lib/config/plugin.php
+++ convead/lib/config/plugin.php
@@ -7,7 +7,7 @@ return array(
     16 => 'img/logo.png'
   ),
   'shop_settings' => true,
-  'version'       => '2.6',
+  'version'       => '2.6.0',
   'vendor'        => 1027096,
   'frontend'      => true,
   'handlers'      => array(
